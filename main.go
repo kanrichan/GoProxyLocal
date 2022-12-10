a56fdb9e76271600474be7f742325061f84d0b3b
Yiwen-Chan <yiwen_chan@foxmail.com>
2022-12-10 13:47:52 +0800

    更新GOPATH的读取方式

--- main.go
+++ main.go
@@ -3,56 +3,65 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"strings"
 )
 
-var BASE_DIR = "C:/Users/xxxxx/go/pkg/mod"
+var BASE_DIR string
+var HOST = "127.0.0.1"
+var PORT = 8080
 
 func init() {
+	gopath, _ := os.LookupEnv("GOPATH")
+	if gopath == "" {
+		panic("the env GOPATH is empty, please set it and rerun again")
+	}
+	BASE_DIR = strings.ReplaceAll(gopath, "\\", "/") + "/pkg/mod"
+	log.Printf("Initialization completed, BASE_DIR is %s\n", BASE_DIR)
+	log.Printf("Please set GOPROXY to http://%s:%d\n", HOST, PORT)
 }
 
 func main() {
-	http.ListenAndServe("127.0.0.1:8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		uri, err := url.PathUnescape(r.RequestURI)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(uri)
-		switch {
-		case strings.HasSuffix(uri, "@v/list"):
-			repo := strings.Split(uri, `/@v`)[0]
-			list, err := version(repo[1:])
+	http.ListenAndServe(fmt.Sprintf("%s:%d", HOST, PORT),
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			uri, err := url.PathUnescape(r.RequestURI)
 			if err != nil {
-				w.WriteHeader(http.StatusNotFound)
-				w.Write([]byte(fmt.Sprintf(`not found: module %s: no matching versions for query "latest"`, repo[1:])))
+				panic(err)
+			}
+			log.Printf("<- %s", uri)
+			switch {
+			case strings.HasSuffix(uri, "@v/list"):
+				repo := strings.Split(uri, `/@v`)[0]
+				list, err := version(repo[1:])
+				if err != nil {
+					w.WriteHeader(http.StatusNotFound)
+					w.Write([]byte(fmt.Sprintf(`not found: module %s: no matching versions for query "latest"`, repo[1:])))
+					return
+				}
+				w.Write([]byte(strings.Join(list, "\n")))
+				return
+			case strings.HasSuffix(uri, "/@latest"):
+				repo := strings.Split(uri, `/@latest`)[0]
+				list, err := version(repo[1:])
+				if err != nil {
+					w.WriteHeader(http.StatusNotFound)
+					w.Write([]byte(fmt.Sprintf(`not found: module %s: no matching versions for query "latest"`, repo[1:])))
+					return
+				}
+				w.Write([]byte(fmt.Sprintf(`{"Version":"%s","Time":"2021-10-08T14:36:13Z"}`, list[len(list)-1])))
 				return
 			}
-			w.Write([]byte(strings.Join(list, "\n")))
-			return
-		case strings.HasSuffix(uri, "/@latest"):
-			repo := strings.Split(uri, `/@latest`)[0]
-			list, err := version(repo[1:])
-			if err != nil {
-				w.WriteHeader(http.StatusNotFound)
-				w.Write([]byte(fmt.Sprintf(`not found: module %s: no matching versions for query "latest"`, repo[1:])))
-				return
-			}
-			w.Write([]byte(fmt.Sprintf(`{"Version":"%s","Time":"2021-10-08T14:36:13Z"}`, list[len(list)-1])))
-			return
-		}
-	}))
+		}),
+	)
 }
 
 func version(module string) ([]string, error) {
 	repoelem := strings.Split(module, `/`)
 	parent := strings.Join(repoelem[:len(repoelem)-1], `/`)
 	name := repoelem[len(repoelem)-1]
-	if strings.Contains(module, "goleveldb") {
-		fmt.Println("goleveldb", repoelem, parent, name, BASE_DIR+"/"+parent)
-	}
 	fd, err := os.ReadDir(BASE_DIR + "/" + parent)
 	if err == nil {
 		var list = make([]string, 0)
